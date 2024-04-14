5536dca1e382a2afd93bf19fb91a4921d69ae8aa
Hubert <hubertlimshingyi@gmail.com>
2024-04-15 02:32:51 +0800

    upload

---
+++ libft/ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hulim <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 15:58:52 by hulim             #+#    #+#             */
+/*   Updated: 2023/09/10 15:35:53 by hulim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*temp;
+	size_t			count;
+
+	count = 0;
+	temp = (unsigned char *) s; 
+	while (count < n)
+	{
+		*temp = c;
+		count++;
+		temp++;
+	}
+	return (s);
+}
