696d8f02289bf45ddaee64e9f0dba82162c09c39
Hubert Shing Yi Lim <hulim@c1r7s8.42singapore.sg>
2024-04-30 20:12:54 +0800

    fixed ft_printf archive file containing archive of libft objs instead of the objs itself

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hulim <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: hulim <hulim@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 15:58:52 by hulim             #+#    #+#             */
-/*   Updated: 2023/09/10 15:35:53 by hulim            ###   ########.fr       */
+/*   Updated: 2024/04/25 00:20:36 by hulim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memset(void *s, int c, size_t n)
 	size_t			count;
 
 	count = 0;
-	temp = (unsigned char *) s; 
+	temp = (unsigned char *) s;
 	while (count < n)
 	{
 		*temp = c;
